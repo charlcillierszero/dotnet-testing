91172a73fc8a432b13ca3bee6786b3a62e81b3b3
Charl Cilliers <charl.cilliers@glacier.co.za>
2023-06-18 15:55:32 +0200

    Remove 0 check for divide for Bad2 implementation

--- Calculators/Implementations/Bad2/Bad2Divide.cs
+++ Calculators/Implementations/Bad2/Bad2Divide.cs
@@ -4,11 +4,6 @@ public class Bad2Divide : IDivide
 {
     public long Divide(long x, long y)
     {
-        if (y == 0)
-        {
-            throw new ArithmeticException("Cannot divide by 0");
-        }
-
         long remainder = x;
         List<long> longs = new List<long>();
         while (remainder >= y)
